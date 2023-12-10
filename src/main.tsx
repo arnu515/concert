3b2eb15f56fb7b93e9addc4f50a18e0152516e16
arnu515 <52203828+arnu515@users.noreply.github.com>
2023-12-10 19:38:03 +0530

    feat: add tailwindcss & prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
-import { render } from 'preact'
-import { App } from './app.tsx'
-import './index.css'
+import { render } from "preact"
+import { App } from "./app.tsx"
+import "@fontsource-variable/inter"
+import "@fontsource-variable/handjet"
+import "./index.postcss"
 
-render(<App />, document.getElementById('app')!)
+render(<App />, document.getElementById("app")!)
