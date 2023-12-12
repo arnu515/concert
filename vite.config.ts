fac1cc98aa41423861510f305e0d0b65e6fc9fb1
arnu515 <52203828+arnu515@users.noreply.github.com>
2023-12-13 05:03:47 +0530

    feat: add import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite"
 import preact from "@preact/preset-vite"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()]
+	plugins: [preact()],
+	resolve: {
+		alias: {
+			$util: resolve("./src/util"),
+			$c: resolve("./src/components"),
+			$r: resolve("./src/routes"),
+			$contexts: resolve("./src/contexts"),
+			$: resolve("./src")
+		}
+	}
 })
