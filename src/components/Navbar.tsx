d8ab22108fb94e48b7a5f5f72556df30a0b71301
arnu515 <52203828+arnu515@users.noreply.github.com>
2023-12-13 09:12:26 +0530

    feat: add avatar of logged in user

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,8 @@ import { useContext } from "preact/hooks"
 import ThemeButton from "$c/ThemeButton"
 import { useComputed } from "@preact/signals"
 import { Badge } from "$c/ui/badge"
+import Avatar from "./Avatar"
+import { Link } from "react-router-dom"
 
 export default function Navbar() {
   const auth = useContext(AuthContext)!
@@ -10,29 +12,40 @@ export default function Navbar() {
 
   return (
     <nav class="flex items-center justify-between border-b border-gray-400 bg-gray-200 px-4 py-2 text-black dark:border-gray-700 dark:bg-gray-800 dark:text-white">
-      <a
-        href="/"
+      <Link
+        to="/"
         class="font-heading text-lg font-semibold text-sky-600 dark:text-blue-600"
         style={{ letterSpacing: ".2ch" }}
       >
         Concert
-      </a>
+      </Link>
       <div class="flex items-center gap-4">
         <ThemeButton />
-        <a href="/" class="font-semibold">
+        <Link to="/" class="font-semibold">
           Explore
-        </a>
+        </Link>
         {user.value && (
-          <a href="/invites" class="font-semibold">
+          <Link to="/invites" class="font-semibold">
             Invites <Badge className="text-sm">3</Badge>
-          </a>
+          </Link>
         )}
-        <a
-          href={user.value ? "/new" : "/signup"}
-          class="border border-gray-700 bg-amber-500 px-2 py-1 font-semibold text-white dark:border-white"
+        <Link
+          to={user.value ? "/new" : "/signup"}
+          class="border border-gray-700 bg-amber-500 px-2 py-1 font-semibold text-white dark:border-gray-200"
         >
           {user.value ? "Create Stage" : "Sign Up"}
-        </a>
+        </Link>
+        {user.value && (
+          <Link to="/settings">
+            <Avatar
+              src={user.value.email!}
+              alt={user.value.email!}
+              fb="ae"
+              w="2rem"
+              h="2rem"
+            />
+          </Link>
+        )}
       </div>
     </nav>
   )
