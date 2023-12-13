82780fc9d9b3f48a79bb70ea5f71fc64338a4e80
arnu515 <52203828+arnu515@users.noreply.github.com>
2023-12-13 07:00:03 +0530

    fix: colors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     <nav class="flex items-center justify-between border-b border-gray-400 bg-gray-200 px-4 py-2 text-black dark:border-gray-700 dark:bg-gray-800 dark:text-white">
       <a
         href="/"
-        class="font-heading text-lg font-semibold text-sky-600"
+        class="font-heading text-lg font-semibold text-sky-600 dark:text-blue-600"
         style={{ letterSpacing: ".2ch" }}
       >
         Concert
