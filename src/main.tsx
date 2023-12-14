3c89c312d0d0d482706548b7d43c62582035bcb3
arnu515 <52203828+arnu515@users.noreply.github.com>
2023-12-15 02:56:11 +0530

    feat: add livekit

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { App } from "./App.tsx"
 import "@fontsource-variable/inter"
 import "@fontsource-variable/handjet"
 import "./index.postcss"
+import "@livekit/components-styles"
+
+  // For livekit
+  ; (window as any).process = { env: { NODE_ENV: "development" } }
 
 createRoot(document.getElementById("app")!).render(
   <StrictMode>
