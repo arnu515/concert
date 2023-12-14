76406ceb2bbd29d82885644292c796dcb42a2421
arnu515 <52203828+arnu515@users.noreply.github.com>
2023-12-14 19:12:05 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import ThemeProvider from "$contexts/ThemeContext"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Index from "$r/page"
+import New from "./routes/new/page"
 import RootLayout from "$r/layouts/Root"
 import RootError from "$r/error"
+import Signup from "./routes/signup/page"
+import newAction from "./routes/new/action"
 
 const router = createBrowserRouter([
 	{
@@ -13,7 +16,10 @@ const router = createBrowserRouter([
 			{
 				path: "/",
 				element: <Index />
-			}
+			},
+			{ path: "/new", element: <New />, action: newAction },
+			{ path: "/signup", element: <Signup /> },
+			{ path: "/login", element: <Signup view="sign_in" /> }
 		]
 	}
 ])
