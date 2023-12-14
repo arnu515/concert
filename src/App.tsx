dee45cff585878fa87635900a6f8ad1a55fdc577
arnu515 <52203828+arnu515@users.noreply.github.com>
2023-12-14 21:11:32 +0530

    feat: go back to react 18.2 and use `@tanstack/react-query` instead.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import ThemeProvider from "$contexts/ThemeContext"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import Index from "$r/page"
 import New from "./routes/new/page"
 import RootLayout from "$r/layouts/Root"
 import RootError from "$r/error"
 import Signup from "./routes/signup/page"
 import newAction from "./routes/new/action"
+
+const queryClient = new QueryClient()
 
 const router = createBrowserRouter([
 	{
@@ -28,7 +32,10 @@ export function App() {
 	return (
 		<>
 			<ThemeProvider>
-				<RouterProvider router={router} />
+				<QueryClientProvider client={queryClient}>
+					<RouterProvider router={router} />
+					<ReactQueryDevtools initialIsOpen={false} />
+				</QueryClientProvider>
 			</ThemeProvider>
 		</>
 	)
