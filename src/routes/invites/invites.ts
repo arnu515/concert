b60b7660cf0c53c5d062eb22ca44748987209083
arnu515 <52203828+arnu515@users.noreply.github.com>
2023-12-15 19:28:30 +0530

    feat: realtime

--- src/routes/invites/invites.ts
+++ src/routes/invites/invites.ts
@@ -1,10 +1,60 @@
+import { useToast } from "$/components/ui/use-toast"
+import { useAuth } from "$/contexts/AuthContext"
 import { Database } from "$/util/dbTypes"
 import supabase from "$/util/supabase"
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { useEffect } from "react"
 
 export function useInvitesCount(acknowledged = true) {
+  const queryKey = ["invites", acknowledged ? "accepted" : "pending", "count"]
+  const client = useQueryClient()
+  const auth = useAuth()
+  const { toast } = useToast()
+
+  useEffect(() => {
+    if (!auth.user) return
+
+    const channel = supabase
+      .channel(`realtime:invites_count`)
+      .on(
+        "postgres_changes",
+        {
+          event: "INSERT",
+          table: "stage_invites",
+          schema: "public",
+          filter: `to_id=eq.${auth.user.id}`
+        },
+        payload => {
+          client.refetchQueries({ queryKey: ["invites"] })
+          if (payload.new?.acknowledged === false) {
+            toast({
+              title: "You have a new invite!",
+              description: "Check your invites page to accept or reject it."
+            })
+          }
+        }
+      )
+      .on(
+        "postgres_changes",
+        {
+          event: "DELETE",
+          table: "stage_invites",
+          schema: "public",
+          filter: `to_id=eq.${auth.user!.id}`
+        },
+        _ => {
+          client.refetchQueries({ queryKey: ["invites"] })
+        }
+      )
+      .subscribe()
+
+    return () => {
+      supabase.removeChannel(channel).catch(console.error)
+    }
+  }, [auth, client, queryKey])
+
   return useQuery({
-    queryKey: ["invites", acknowledged ? "accepted" : "pending", "count"],
+    queryKey,
     queryFn: async ({ signal }) => {
       const { count, error } = await supabase
         .from("stage_invites")
